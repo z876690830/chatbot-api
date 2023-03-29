073b16e8bb53fed98298924f7b4bfd27454dfb12
z876690830 <876690830@qq.com>
2023-03-29 21:37:43 +0800

    init project

---
+++ chatbot-api-infrastructure/src/main/java/com/icbc/chatbot/api/infrastructure/package-info.java
@@ -0,0 +1,1 @@
+package com.icbc.chatbot.api.infrastructure;
---
+++ chatbot-api-interfaces/src/main/java/com/icbc/chatbot/api/ApiApplication.java
@@ -0,0 +1,12 @@
+package com.icbc.chatbot.api;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
